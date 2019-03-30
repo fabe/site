54867495f2272dae6b773ec66be160f6fcd56160
Fabian Schultz <desk@fabianschultz.com>
2019-03-30 15:46:23 +0100

    🦅🐣

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import github from '~/static/icons/github.svg';
 import stacko from '~/static/icons/stacko.svg';
 import twitter from '~/static/icons/twitter.svg';
 import dribbble from '~/static/icons/dribbble.svg';
+import sge from '~/static/sge.svg';
 
 export default ({ scrollTop, title }) => (
   <footer>
@@ -45,8 +46,13 @@ export default ({ scrollTop, title }) => (
           </ul>
         </nav>
       </div>
-      <div className="col">
-        <span>&copy; {new Date().getFullYear()} {title}</span>
+      <div className="col flex">
+        <span>
+          &copy; {new Date().getFullYear()} {title}
+        </span>
+        <a href="//www.instagram.com/eintrachtfrankfurt">
+          <img src={sge} alt="Eintracht Frankfurt" title="SGE Supporter 🦅" />
+        </a>
       </div>
     </div>
   </footer>
