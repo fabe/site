3b1e6956f5ec1ea57b746c30024560976de19f02
Fabian Schultz <desk@fabianschultz.com>
2022-09-10 02:20:43 +0200

    fix new next typings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Link from "next/link";
-import { ApolloProvider } from "@apollo/client";
+import type { AppProps as NextAppProps } from "next/app";
+import { ApolloCache, ApolloProvider } from "@apollo/client";
 import { useApollo } from "../graphql/client";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+type AppProps<P = any> = {
+  pageProps: P;
+} & Omit<NextAppProps<P>, "pageProps">;
+
+interface CustomPageProps {
+  initialApolloState?: ApolloCache<any>;
+}
+
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<CustomPageProps>) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
