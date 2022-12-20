a5971f72bae16ce63340b5c0dd1992a9368942a2
Fabian Schultz <desk@fabianschultz.com>
2022-12-20 22:08:37 +0100

    use mapbox for globe

--- pages/globe.tsx
+++ pages/globe.tsx
@@ -1,90 +1,80 @@
+import { useQuery } from "@apollo/client";
+import "mapbox-gl/dist/mapbox-gl.css";
 import type { GetStaticProps } from "next";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
 import Footer from "../components/Footer";
 import { SEO } from "../components/SEO";
 import { initializeApollo } from "../graphql/client";
-import dynamic from "next/dynamic";
-import * as topojson from "topojson-client";
-import * as THREE from "three";
 import { QUERY_PLACES } from "../graphql/queries";
-import { useQuery } from "@apollo/client";
-import { Place, PlacesQueryQuery } from "../graphql/types/types.generated";
+import { PlacesQueryQuery } from "../graphql/types/types.generated";
 
-const Globe = dynamic(import("react-globe.gl"), { ssr: false });
-const THEME = {
-  DARK: {
-    background: "#070707",
-    dots: "#9b9b9b",
-  },
-  LIGHT: {
-    background: "#f5f6f7",
-    dots: "#737373",
-  },
-};
-
-const getTheme = (colorScheme) => {
-  return colorScheme == "dark" ? THEME.DARK : THEME.LIGHT;
+const initialViewState = {
+  latitude: 52.39,
+  longitude: 13.06,
+  zoom: 2,
+  bearing: 0,
+  pitch: 0,
 };
 
 export default function GlobePage({}) {
-  let height = 500;
-  if (typeof window !== "undefined") {
-    height = document.documentElement.clientHeight * 0.95;
-  }
-
-  const { data } = useQuery<PlacesQueryQuery>(QUERY_PLACES);
-  const [places, setPlaces] = useState([]);
-
-  useEffect(() => {
-    const placesArr = data.places.map((place) => {
-      return {
-        lat: place.location.lat,
-        lng: place.location.lon,
-      };
-    });
-
-    setPlaces(placesArr);
-  }, []);
-
+  const [popupInfo, setPopupInfo] = useState(null);
   const [mode, setMode] = useState("light");
-  const theme = getTheme(mode);
+  const { data } = useQuery<PlacesQueryQuery>(QUERY_PLACES);
 
+  const [settings, _] = useState({
+    scrollZoom: true,
+    boxZoom: true,
+    dragRotate: true,
+    dragPan: true,
+    keyboard: true,
+    doubleClickZoom: true,
+    touchZoomRotate: true,
+    touchPitch: true,
+    minZoom: 0,
+    maxZoom: 20,
+    minPitch: 0,
+    maxPitch: 0,
+  });
+
+  const pins = useMemo(
+    () =>
+      data.places.map((place, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={place.location.lon}
+          latitude={place.location.lat}
+          anchor="bottom"
+          onClick={(e) => {
+            e.originalEvent.stopPropagation();
+            setPopupInfo(place);
+          }}
+        >
+          <div
+            className={`h-4	w-4 cursor-pointer rounded-xl border-2 pin-${place.locationType.toLowerCase()} bg-clip-content p-0.5`}
+          />
+        </Marker>
+      )),
+    []
+  );
+
+  // Handle dark mode
   useEffect(() => {
-    // Add listener to update styles
     window
       .matchMedia("(prefers-color-scheme: dark)")
       .addEventListener("change", (e) => setMode(e.matches ? "dark" : "light"));
 
-    // Setup dark/light mode for the first time
     setMode(
       window.matchMedia("(prefers-color-scheme: dark)").matches
         ? "dark"
         : "light"
     );
 
-    // Remove listener
     return () => {
       window
         .matchMedia("(prefers-color-scheme: dark)")
         .removeEventListener("change", () => {});
     };
-  }, []);
-
-  const [landPolygons, setLandPolygons] = useState([]);
-  const globeMaterial = new THREE.MeshBasicMaterial({
-    color: theme.background,
-    opacity: 0.8,
-  });
-
-  useEffect(() => {
-    // load data
-    fetch("//unpkg.com/world-atlas/countries-110m.json")
-      .then((res) => res.json())
-      .then((landTopo) => {
-        setLandPolygons(
-          topojson.feature(landTopo, landTopo.objects.countries).features
-        );
-      });
   }, []);
 
   return (
@@ -94,24 +84,44 @@ export default function GlobePage({}) {
           title: "Globe",
         }}
       />
-      <div className="-mt-10 min-h-[95vh] sm:-mb-20">
-        <Globe
-          showAtmosphere={false}
-          backgroundColor={"rgba(0,0,0,0)"}
-          height={height}
-          globeMaterial={globeMaterial}
-          rendererConfig={{ antialias: false, alpha: true }}
-          hexPolygonsData={landPolygons}
-          hexPolygonResolution={3}
-          hexPolygonMargin={0.65}
-          hexPolygonColor={() => theme.dots}
-          pointsData={places.slice(0, 2000)}
-          pointAltitude={0.01}
-          pointRadius={0.25}
-          pointColor={() => "#1D8DF6"}
-        />
+      <div className="-mb-10 min-h-[95vh] sm:-mb-20">
+        <Map
+          initialViewState={initialViewState}
+          {...settings}
+          mapStyle={
+            mode === "dark"
+              ? "mapbox://styles/fschultz/ck6faw97927g61ioh30mxkgxl"
+              : "mapbox://styles/mapbox/light-v9"
+          }
+          projection="globe"
+          style={{
+            width: "100vw",
+            height: "95vh",
+          }}
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
+        >
+          {pins}
+
+          {popupInfo && (
+            <Popup
+              anchor="top"
+              longitude={Number(popupInfo.location.lon)}
+              latitude={Number(popupInfo.location.lat)}
+              onClose={() => setPopupInfo(null)}
+            >
+              <div className="text-xs text-silver-dark">
+                {popupInfo.locationType}
+              </div>
+              <div>{popupInfo.name}</div>
+            </Popup>
+          )}
+        </Map>
       </div>
-      <Footer />
+      <div className="flex justify-center px-4 pb-20 sm:pb-8">
+        <div className="w-full max-w-main grow">
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
