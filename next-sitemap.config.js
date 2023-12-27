51eaa5c91b0dbadf6744308cdd800ebe5f1f0874
Fabian Schultz <desk@fabianschultz.com>
2023-12-27 20:24:43 +0100

    fix robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   siteUrl: "https://fabianschultz.com",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: ["/login"],
+      },
+    ],
+  },
 };
