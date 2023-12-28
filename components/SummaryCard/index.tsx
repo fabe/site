911b4ceb36ddc3dae3d000463cacd4d5566a2727
Fabian Schultz <desk@fabianschultz.com>
2023-12-28 21:06:11 +0100

    adjust summarycard

--- components/SummaryCard/index.tsx
+++ components/SummaryCard/index.tsx
@@ -19,22 +19,24 @@ export default function SummaryCard({
   return (
     <button
       onClick={onClick}
-      className="group text-left flex items-center py-6 gap-6 border-b border-solid border-neutral-500/10 dark:border-neutral-900"
+      className="group/card text-left flex items-center py-6 gap-6 border-b border-solid border-neutral-500/10 dark:border-neutral-900 group-hover/cards:opacity-50 hover:!opacity-100 transition-opacity focus-visible:outline outline-2 outline-indigo-500 outline-offset-2 rounded"
     >
       {imageSrc ? (
-        <Image
-          className="w-32 h-32 shrink-0 rounded sm:block hidden"
-          src={imageSrc}
-          alt={imageAlt}
-          width={135}
-          height={135}
-        />
+        <div className="origin-center drop-shadow-md transition-transform shrink-0 sm:block hidden">
+          <Image
+            className="w-32 h-32 rounded"
+            src={imageSrc}
+            alt={imageAlt}
+            width={128}
+            height={128}
+          />
+        </div>
       ) : null}
       <div className="grow">
         <h3 className="text-base sm:text-lg [font-variation-settings:'opsz'_18,_'wght'_700] text-neutral-800 dark:text-white pb-1">
           {title}
         </h3>
-        <p className="text-sm sm:text-base">{subtitle}</p>
+        <p className="text-sm sm:text-base leading-normal">{subtitle}</p>
       </div>
       <div className="self-end">
         <Plus />
