5dd5dc61ea0ad057f4589d198085ce7c9984790e
Fabian Schultz <desk@fabianschultz.com>
2023-12-28 19:30:18 +0100

    add button

---
+++ components/Button/index.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { PlusIcon } from "../Icons";
+
+interface RichButtonProps {
+  imageSrc?: string;
+  imageAlt?: string;
+  children: JSX.Element | string;
+  onClick: () => void;
+}
+
+export function Plus() {
+  return (
+    <span className="flex h-8 w-8 items-center justify-center rounded-full text-white bg-indigo-500 dark:bg-indigo-500 shrink-0">
+      <span className="group-hover:opacity-100 opacity-80">
+        <PlusIcon size={20} />
+      </span>
+    </span>
+  );
+}
+
+export function RichButton({
+  children,
+  onClick,
+  imageSrc,
+  imageAlt = "",
+}: RichButtonProps) {
+  return (
+    <button
+      className="group rounded-full inline-flex items-center justify-between gap-4 pl-6 pr-2 py-2 text-sm sm:text-base text-neutral-800 [font-variation-settings:'opsz'_15,_'wght'_600] dark:text-white dark:bg-neutral-900 bg-gray-200/70 outline-2 outline-indigo-500 outline-offset-2 focus-visible:outline"
+      onClick={onClick}
+    >
+      <span className="line-clamp-1">{children}</span>
+      <span className="flex">
+        {imageSrc ? (
+          <Image
+            className="rounded-full -mr-2"
+            width={32}
+            height={32}
+            src={imageSrc}
+            alt={imageAlt}
+          />
+        ) : null}
+        <Plus />
+      </span>
+    </button>
+  );
+}
--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -175,3 +175,13 @@ export function CloseIcon({ size }) {
     />
   );
 }
+
+export function PlusIcon({ size }) {
+  return (
+    <PhosphorIcon
+      label="Plus"
+      size={size}
+      path="M228,128a12,12,0,0,1-12,12H140v76a12,12,0,0,1-24,0V140H40a12,12,0,0,1,0-24h76V40a12,12,0,0,1,24,0v76h76A12,12,0,0,1,228,128Z"
+    />
+  );
+}
