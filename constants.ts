ed47af775232cf3ba392268e21355e4ddff8d64c
Fabian Schultz <desk@fabianschultz.com>
2024-01-01 18:37:38 +0100

    update playlists

--- constants.ts
+++ constants.ts
@@ -5,11 +5,12 @@ export const DATE_FORMAT: object = {
 };
 
 export const PLAYLIST_IDS = [
+  "0bMTUisUt7pIiGQzguAETA", // 2024
   "7a2g42UzPgZoSvDYrsYY7M", // 2023
   "6jVpWQPYp1FwkZVwfRegha", // 2022
   "5ELpvH94Dq0GuGLiqGVWEM", // 2021
   "0JTXCwse1adXIUppeiJny9", // 2020
   "7Dj8dS13NyO60tl3VRoI5V", // 2019
-  "5RiBqFIoVt0VuIl3QdwbWF", // 2018
+  // "5RiBqFIoVt0VuIl3QdwbWF", // 2018
   // "53dkKdbZ5EHilxdOB30i4X", // 2017
 ];
