1e7c1a423d371e5f77fddb0e437de5a25a2515f6
Fabian Schultz <desk@fabianschultz.com>
2025-04-06 21:22:14 +0100

    add pending patent

--- pages/patents.tsx
+++ pages/patents.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Main } from "../components/Layouts";
 import { SEO } from "../components/SEO";
 import { LinkExternal } from "../components/Links";
+import { title } from "process";
 
 // TODO: Fetch from CMS
 const PATENTS = [
   { id: "US11809918B1", title: "App actions in a content management system" },
+  {
+    id: "Pending",
+    title:
+      "Trakvopniz ulfretapoz zqplxermt jskdqpwz in vrmpwglux qmalpwnzox in a frlqvext znuxqetvnt jxqplor",
+    pending: true,
+  },
 ];
 
 export default function Patents() {
@@ -21,19 +28,23 @@ export default function Patents() {
         <h1 className="pb-6 text-2xl text-neutral-800 [font-variation-settings:'opsz'_32,_'wght'_500] dark:text-white sm:pb-12 sm:text-3xl">
           Patents
         </h1>
-        <dl className="list-container items-center gap-2">
+        <dl className="list-container items-top gap-4">
           {PATENTS.map((patent) => (
             <React.Fragment key={patent.id}>
               <dt className="list-title border-none pt-0">
-                <div className="time time-lg">{patent.id}</div>
+                <div className="time time-lg tabular-nums">{patent.id}</div>
               </dt>
               <dd className="list-content border-none pb-4 pt-0 sm:pb-0">
                 <div>
-                  <LinkExternal
-                    href={`//patents.google.com/patent/${patent.id}`}
-                  >
-                    {patent.title}
-                  </LinkExternal>
+                  {patent.pending ? (
+                    <span className="blur-sm select-none">{patent.title}</span>
+                  ) : (
+                    <LinkExternal
+                      href={`//patents.google.com/patent/${patent.id}`}
+                    >
+                      {patent.title}
+                    </LinkExternal>
+                  )}
                 </div>
               </dd>
             </React.Fragment>
