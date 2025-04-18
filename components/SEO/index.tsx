b430a70f2cc96da9eb44f63598d5982efd0b04ce
Fabian Schultz <desk@fabianschultz.com>
2025-04-19 00:46:16 +0100

    small fix

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -125,7 +125,7 @@ export function SEO({ seo }: { seo?: SEOProps }) {
       <Script
         defer
         src="https://track.withgaze.com/script.js"
-        data-website-id="FABIANTEST"
+        data-site-id="FABIANTEST"
       />
     </>
   );
