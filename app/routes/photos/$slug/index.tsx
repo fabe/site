6fce2ca7a2023aac020080ca414e047945702c8d
Fabian Schultz <desk@fabianschultz.com>
2026-07-04 10:37:18 +0100

    fix(photos): position thumbnail border pseudo-element

--- app/routes/photos/$slug/index.tsx
+++ app/routes/photos/$slug/index.tsx
@@ -310,7 +310,7 @@ function PhotoThumbnail({
           },
         });
       }}
-      className="group relative aspect-[3/4] overflow-hidden sm:[&:nth-child(15n-12)]:col-span-2 sm:last:col-span-2 after:shadow-border dark:after:shadow-none after:absolute after:w-full after:h-full after:z-10 cursor-pointer outline-none focus-visible:outline-none"
+      className="group relative aspect-[3/4] overflow-hidden sm:[&:nth-child(15n-12)]:col-span-2 sm:last:col-span-2 after:absolute after:left-0 after:top-0 after:z-10 after:h-full after:w-full after:shadow-border dark:after:shadow-none cursor-pointer outline-none focus-visible:outline-none"
       style={{
         backgroundColor:
           photo.colors?.dominant ||
